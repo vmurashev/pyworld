b5c75f9b8ac6ecc043d97d92e9458ad3682eafea
vmurashev <vitaly.murashev@gmail.com>
2018-04-09 00:05:56 +0300

    added module 'pycurl'

---
+++ publish/minibuild.mk
@@ -0,0 +1,9 @@
+#include "../../cpython/publish/bundle/bundled_python.inc"
+
+module_type = 'composite'
+module_name = 'bundled_python_world'
+
+composite_spec += [
+  ['${@project_root}/cpython/build/stdlib', PYCORE_PROPERTIES],
+  ['../pycurl', PYMOD_PROPERTIES_SITE],
+]
---
+++ pycurl/minibuild.mk
@@ -0,0 +1,41 @@
+#include "../../cpython/pymod_shared.inc"
+
+module_type = 'lib-shared'
+module_name = 'pycurl'
+
+build_list = [
+  'docstrings.c',
+  'easy.c',
+  'module.c',
+  'multi.c',
+  'oscompat.c',
+  'pythoncompat.c',
+  'share.c',
+  'stringcompat.c',
+  'threadsupport.c',
+]
+
+export = ['initpycurl']
+
+src_search_dir_list = [
+  'src'
+]
+
+include_dir_list = [
+  '${@project_root}/cpython/vendor/Include',
+  '${@project_root}/openssl/include',
+  '${@project_root}/curl/include',
+]
+
+lib_list = [
+  '${@project_root}/cpython/build/core',
+  '${@project_root}/curl',
+  '${@project_root}/openssl/build/ssl_static',
+  '${@project_root}/openssl/build/crypto_static',
+  '${@project_root}/zlib',
+]
+
+definitions += ['PYCURL_VERSION="7.43.0"', 'NDEBUG', 'HAVE_CURL_OPENSSL', 'HAVE_CURL_SSL']
+
+prebuilt_lib_list_windows = ['crypt32','ws2_32', 'advapi32', 'user32']
+prebuilt_lib_list_linux = ['dl', 'pthread', 'rt']
